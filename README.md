1642c885e06c2f355551a8212effee4317634a79
Durgesh Ujjainkar <durgeshujjainkar786@gmail.com>
2025-03-01 03:15:56 +0530

    setup

---
+++ README.md
@@ -0,0 +1,1 @@
+## end tio END mlproject
---
+++ setup.py
@@ -0,0 +1,77 @@
+# #what is setup.py
+
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
+
+
+# from flask import Flask,request,render_template
+# import numpy as np
+# import pandas as pd
+
+# from sklearn.preprocessing import StandardScaler
+# from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+# application=Flask(__name__)
+
+# app=application
+
+# ## Route for a home page
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html') 
+
+# @app.route('/predictdata',methods=['GET','POST'])
+# def predict_datapoint():
+#     if request.method=='GET':
+#         return render_template('home.html')
+#     else:
+#         data=CustomData(
+#             gender=request.form.get('gender'),
+#             race_ethnicity=request.form.get('ethnicity'),
+#             parental_level_of_education=request.form.get('parental_level_of_education'),
+#             lunch=request.form.get('lunch'),
+#             test_preparation_course=request.form.get('test_preparation_course'),
+#             reading_score=float(request.form.get('writing_score')),
+#             writing_score=float(request.form.get('reading_score'))
+
+#         )
+#         pred_df=data.get_data_as_data_frame()
+#         print(pred_df)
+#         print("Before Prediction")
+
+#         predict_pipeline=PredictPipeline()
+#         print("Mid Prediction")
+#         results=predict_pipeline.predict(pred_df)
+#         print("after Prediction")
+#         return render_template('home.html',results=results[0])
+    
+
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0")        
